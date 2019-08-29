dd446a9515d597564125c6f137e53ddc5d58ed83
akshat <akshatbhargavadeepa@gmail.com>
2019-08-29 08:56:54 +0530

    added localstorage-keys file and added authentication interceptor in axios instance

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import Vue from 'vue';
 import axios from 'axios';
 import Buefy from 'buefy';
+import localforage from 'localforage';
 import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'buefy/dist/buefy.css';
 import './registerServiceWorker';
+import { AUTH_TOKEN } from './localstorage-keys';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/api/',
 });
+
+// http interceptor to add `authorization` field in requests
+axios.interceptors.request.use(async () => ({
+  headers: {
+    authorization: `Bearer ${await localforage.getItem(AUTH_TOKEN)}`,
+  },
+}));
 
 Vue.use(VueAxios, axiosInstance);
 
