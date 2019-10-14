963a6e9fe6a6b9fdd7a885b7b481c60ab15bda0e
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-10-14 10:49:28 +0530

    ag-grid added completely with action buttons working

---
+++ src/components/TableActionButtons.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="margin-top: 7px">
+    <a
+      class="button is-primary is-small is-outlined"
+      style="margin-right: 10px"
+      @click="onButtonClick('edit')"
+    >
+      <span>Edit</span>
+      <span class="icon is-small">
+        <i class="fas fa-edit"></i>
+      </span>
+    </a>
+    <a class="button is-danger is-small is-outlined" @click="onButtonClick('delete')">
+      <span>Delete</span>
+      <span class="icon is-small">
+        <i class="fas fa-times"></i>
+      </span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableActionButtons',
+  methods: {
+    onButtonClick(type) {
+      this.params.frameworkComponentWrapper.parent.$parent.onTableButtonClick(type, this.params.data);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
