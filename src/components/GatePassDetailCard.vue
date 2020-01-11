d4eba1f07d928af844314140d0bac50e63bebc15
sakshi srivastava <sakshisrivastava413@gmail.com>
2020-01-11 16:02:18 +0530

    added otp component in gatepass card

--- src/components/GatePassDetailCard.vue
+++ src/components/GatePassDetailCard.vue
@@ -1,52 +1,83 @@
 <template>
-    <div class="card">
-      <div class="card-content">
-        <div class="columns col">
-          <div class="column inner-col is-3">
-            <p class="title is-5">Sakshi Srivastava</p>
-            <p class="subtitle is-6">VI - A, 45</p>
-          </div>
+  <div class="card">
+    <div class="card-content">
+      <div class="columns col">
+        <div class="column inner-col is-3">
+          <p class="title is-5">Sakshi Srivastava</p>
+          <p class="subtitle is-6">VI - A, 45</p>
+        </div>
 
-          <div class="column inner-col is-3">
-            <p class="picked-title title is-5 m-0">Rekha Srivastava</p>
-            <p class="is-6">Niece, 8585858593</p>
-            <!-- <p class="subtitle is-6 is-inline">Verified By:</p>
-            <p class="title is-6 is-inline">Father</p>-->
-          </div>
+        <div class="column inner-col is-3">
+          <p class="picked-title title is-5 m-0">Rekha Srivastava</p>
+          <p class="is-6">Niece, 8585858593</p>
+          <!-- <p class="subtitle is-6 is-inline">Verified By:</p>
+          <p class="title is-6 is-inline">Father</p>-->
+        </div>
 
-          <div class="column inner-col is-2">
-            <!-- <p class="picked-title title is-6 m-0">Reason for picking:</p> -->
-            <p class="is-6">Family function at nani's home</p>
-          </div>
+        <div class="column inner-col is-2">
+          <!-- <p class="picked-title title is-6 m-0">Reason for picking:</p> -->
+          <p class="is-6">Family function at nani's home</p>
+        </div>
 
-          <div class="column inner-col is-2">
-            <div class="right-container">
-              <!-- <div>Fri, 26 Jan, 2019 (10:45)</div> -->
-              <!-- <div class="mt-1"></div> -->
-              <p class="title is-6">
-                <b-tag rounded type="is-warning">Awaiting</b-tag>
-              </p>
-            </div>
+        <div class="column inner-col is-2">
+          <div class="right-container">
+            <!-- <div>Fri, 26 Jan, 2019 (10:45)</div> -->
+            <!-- <div class="mt-1"></div> -->
+            <p class="title is-6">
+              <b-tag rounded type="is-warning">Awaiting</b-tag>
+            </p>
           </div>
+        </div>
 
-          <div class="column inner-col is-2">
-            <!-- <div class="status">
+        <div class="column inner-col is-2">
+          <!-- <div class="status">
               <b-icon pack="fas" icon="exclamation-triangle" size="is-small" type="is-warning"></b-icon>
-            </div>-->
-            <div class="btn-details">
-              <b-button type="is-primary" size="is-small">Verify OTP</b-button>
-            </div>
-            <div class="btn-details">
-              <a class="cancel">Cancel Request</a>
-            </div>
+          </div>-->
+          <div class="btn-details">
+            <otp-input />
+          </div>
+          <div class="btn-details">
+            <a class="cancel" @click="deleteRequest">Cancel Request</a>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import OtpInput from './common/OtpInput.vue';
+
+export default {
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  components: {
+    'otp-input': OtpInput,
+  },
+  methods: {
+    deleteRequest() {
+      const { dialog, snackbar } = this.$buefy;
+      dialog.confirm({
+        title: 'Deleting Gatepass Request',
+        message:
+          'Are you sure you want to <b>delete</b> this gatepass request? This action cannot be undone.',
+        confirmText: 'Delete Request',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => {
+          this.loading = true;
+          setTimeout(() => {
+            snackbar.open('Request deleted!');
+            this.loading = false;
+          }, 2000);
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style>
@@ -72,5 +103,15 @@ export default {};
 }
 .inner-col {
   padding: 0px !important;
+}
+.otp {
+  padding-right: 2px !important;
+}
+.otp-button {
+  width: 50%;
+  height: 70%;
+}
+.otp-input {
+  height: 28px !important;
 }
 </style>
