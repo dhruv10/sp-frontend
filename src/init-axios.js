9ea3303605e0251783f016dbebfb5374a649dc8f
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2021-01-07 04:26:53 +0530

    feat: shifted to new apis

--- src/init-axios.js
+++ src/init-axios.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { AUTH_TOKEN } from './localstorage-keys';
 
+// https://pacific-spire-65720.herokuapp.com/
+// https://studentpress-backend.herokuapp.com/api/
 const axiosInstance = axios.create({
-  baseURL: 'https://studentpress-backend.herokuapp.com/api/',
+  baseURL: 'https://pacific-spire-65720.herokuapp.com/api/',
 });
 
 // http interceptor to add `authorization` field in requests
